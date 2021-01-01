c04a39d22000d16f38efb2b53510cc93cb79a4f0
Simon Levesque <simon@foilen.com>
2021-01-01 16:32:33 -0500

    Update copyrights

--- src/main/java/com/foilen/infra/docker/manager/tasks/MonitoringTask.java
+++ src/main/java/com/foilen/infra/docker/manager/tasks/MonitoringTask.java
@@ -1,7 +1,7 @@
 /*
     Foilen Infra Docker Manager
     https://github.com/foilen/foilen-infra-docker-manager
-    Copyright (c) 2017-2020 Foilen (http://foilen.com)
+    Copyright (c) 2017-2021 Foilen (http://foilen.com)
 
     The MIT License
     http://opensource.org/licenses/MIT
